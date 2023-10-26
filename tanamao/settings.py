840e69a12a6095c9158cda2d84b5f4de4a5bc94f
LucasFDias <lucasfdiasdev@gmail.com>
2023-10-26 16:23:26 -0300

    feat: cart_item, add, remove, clear and count

--- tanamao/settings.py
+++ tanamao/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
-    'store'
+    'store',
+    'carts'
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links'
+                'category.context_processors.menu_links',
+                'carts.context_processors.counter'
             ],
         },
     },
